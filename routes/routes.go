434e87e5e123ec4680ceb5c8612e7cbfe7ca401e
balinux <ryojuniyantara@gmail.com>
2024-08-08 18:33:18 +0800

    test route

--- routes/routes.go
+++ routes/routes.go
@@ -11,6 +11,7 @@ func SetupRouter(haircutController *controllers.HaircutController) *gin.Engine {
 	v1 := router.Group("/api/v1")
 	{
 		v1.GET("/haircuts", haircutController.GetAllHaircuts)
+		v1.GET("/haircuts2", haircutController.GetAllHaircuts)
 		v1.POST("/haircuts", haircutController.CreateHaircut)
 		v1.GET("/haircuts/:id", haircutController.GetHaircut)
 	}
